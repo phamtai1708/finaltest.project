39d95728758dcced0f302deb14b735f5dbcc11eb
phamtai1708 <phamtai.tind46@gmail.com>
2024-07-12 13:51:55 +0700

    router login, register

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,19 @@ import EnterOTP from './EnterOTP/EnterOTP.jsx'
 import Home from './Components/Home.jsx'
 import Checkout from './Checkout/Checkout.jsx'
 import ProductInfo from './Components/ProductInfo.jsx'
-
+import ScrollToTop from './Components/ScrollToTop.jsx'
+import Login from './Components/login.jsx'
+import Register from './Components/Register.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ProductProvider>
   <BrowserRouter>
+  <ScrollToTop />
   <Routes>
     <Route path='/' element={<Home />}/>
+    <Route path='/Login' element={< Login/>} />
+    <Route path='/Register' element={< Register/>} />
     <Route path='/EnterOTP' element={< EnterOTP/>} />
     <Route path='/Checkout' element={<Checkout />}/>
     <Route path="/product/:name" element={<ProductInfo />} />
